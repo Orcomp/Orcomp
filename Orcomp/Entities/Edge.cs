d0e401a0fb5d5e4cf5081cb29ca64b1392cb13b8
orcomp <orcomp@server>
2012-10-10 09:31:47 +1100

    DateRangeEfficiency and Tests

---
+++ Orcomp/Entities/Edge.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Orcomp.Interfaces;
+
+namespace Orcomp.Entities
+{
+    public class Edge<T> where T : IDateRange
+    {
+        public T DateRange { get; private set; }
+
+        public EdgeType EdgeType { get; private set; }
+
+        public DateTime DateTime { get { return EdgeType == EdgeType.Start ? DateRange.StartTime : DateRange.EndTime;}}
+
+        public Edge(T dateRange, EdgeType edgeType)
+        {
+            DateRange = dateRange;
+            EdgeType = edgeType;
+        }
+    }
+}
---
+++ Orcomp/Extensions/DateRangeExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Orcomp.Entities;
+
+namespace Orcomp.Extensions
+{
+    public static class DateRangeExtensions
+    {
+
+        public static DateRange AccountForEfficiencies(this DateRange dateRange, List<DateRangeEfficiency> calendarEfficiencies, FixedEdge fixedEdge = FixedEdge.Start)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
