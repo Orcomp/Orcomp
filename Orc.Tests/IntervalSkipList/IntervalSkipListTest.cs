0154821b8c169769dcf8299022f349ae080c3137
Petro Sidlovskyy <sidlovskyy@gmail.com>
2012-10-25 22:47:22 +0300

    IntervalSkipList tests moved to NUnit. IntervalSkipList benchmark tests refactored and moved to separate file.

--- Orc.Tests/IntervalSkipList/IntervalSkipListTest.cs
+++ Orc.Tests/IntervalSkipList/IntervalSkipListTest.cs
@@ -1,21 +1,16 @@
 ﻿namespace Orc.Tests.IntervalSkipList
 {
-    using System;
-    using System.Diagnostics;
-    using System.Linq;
-    using System.Text;
-
-    using Microsoft.VisualStudio.TestTools.UnitTesting;
-
     using System.Collections.Generic;
+
+    using NUnit.Framework;
 
     using Orc.Entities;
     using Orc.Entities.IntervalSkipList;
 
-    [TestClass]
+    [TestFixture]
     public class IntervalSkipListTest
     {
-        [TestMethod]
+        [Test]
         public void TestSearch()
         {
             List<Interval<int>> intervals = new List<Interval<int>>();
@@ -74,79 +69,6 @@
 
             intersection1 = it2.Search(new Interval<int>(1780, 1790));
             Assert.AreEqual(2, intersection1.Count);
-        }
-
-        [TestMethod]
-        public void Benchamrk()
-        {
-            var date = DateTime.Now;
-
-            int numberOfIntervals = 10000;
-
-            var intervals = GetDateRangesAllDescendingEndTimes(date, numberOfIntervals).ToList();
-
-            var sb = new StringBuilder();
-            Stopwatch sp = Stopwatch.StartNew();
-
-            var it = new IntervalSkipList<DateTime>(intervals);
-
-            sp.Stop();
-            sb.AppendLine("Time taken to build tree: " + sp.ElapsedMilliseconds);
-
-            sp.Reset();
-            sp.Start();
-
-
-            var result = it.Search(new Interval<DateTime>(date, date.AddMinutes(numberOfIntervals)));
-
-            sp.Stop();
-            sb.AppendLine("Time taken for first: " + sp.ElapsedMilliseconds);
-
-            sp.Reset();
-            sp.Start();
-
-            var result2 = it.Search(new Interval<DateTime>(date.AddMinutes(-1), date.AddMinutes(1)));
-
-            sp.Stop();
-            sb.AppendLine("Time taken for second: " + sp.ElapsedMilliseconds);
-
-            sp.Reset();
-            sp.Start();
-
-            var result3 = it.Search(new Interval<DateTime>(date.AddMinutes(-numberOfIntervals), date.AddMinutes(numberOfIntervals)));
-
-            sp.Stop();
-            sb.AppendLine("Time taken for Third: " + sp.ElapsedMilliseconds);
-            sp.Reset();
-            sp.Start();
-
-            var result4 = it.Search(new Interval<DateTime>(date.AddMinutes(numberOfIntervals - 1), date.AddMinutes(numberOfIntervals)));
-
-            sp.Stop();
-            sb.AppendLine("Time taken for Fourth: " + sp.ElapsedMilliseconds);
-
-            Assert.AreEqual(numberOfIntervals, result.Count);
-
-            Assert.AreEqual(numberOfIntervals, result2.Count);
-
-            Assert.AreEqual(numberOfIntervals, result3.Count);
-
-            Assert.AreEqual(2, result4.Count);
-
-            var test = sb.ToString();
-        }
-
-        public static IEnumerable<Interval<DateTime>> GetDateRangesAllDescendingEndTimes(DateTime date, int count)
-        {
-
-            var dateRanges = new Interval<DateTime>[count];
-
-            for (int i = 1; i <= count; i++)
-            {
-                dateRanges[i - 1] = new Interval<DateTime>(date.AddMinutes(-i), date.AddMinutes(i));
-            }
-
-            return new List<Interval<DateTime>>(dateRanges).OrderBy(x => x.Min);
         }
     }
 }
