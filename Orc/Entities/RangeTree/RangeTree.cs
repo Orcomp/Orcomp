3b6a972685acdd703999d87f5b44c79c190bd6fd
orcomp <orcomp@server>
2012-10-27 08:49:05 +1100

    Copied RangeTree implementation from https://github.com/jandorfer/RangeTree

---
+++ Orc/Entities/RangeTree/RangeTree.cs
@@ -0,0 +1,163 @@
+﻿namespace Orc.Entities.RangeTree
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    /// <summary>
+    /// The standard range tree implementation. Keeps a root node and
+    /// forwards all queries to it.
+    /// Whenenver new items are added or items are removed, the tree 
+    /// goes "out of sync" and is rebuild when it's queried next.
+    /// </summary>
+    /// <typeparam name="TKey">The type of the range.</typeparam>
+    /// <typeparam name="T">The type of the data items.</typeparam>
+    public class RangeTree<TKey, T> : IRangeTree<TKey, T>
+        where TKey : IComparable<TKey>
+        where T : IRangeProvider<TKey>
+    {
+        private RangeTreeNode<TKey, T> _root;
+        private List<T> _items;
+        private bool _isInSync;
+        private bool _autoRebuild;
+        private IComparer<T> _rangeComparer;
+
+        /// <summary>
+        /// Whether the tree is currently in sync or not. If it is "out of sync"
+        /// you can either rebuild it manually (call Rebuild) or let it rebuild
+        /// automatically when you query it next.
+        /// </summary>
+        public bool IsInSync
+        {
+            get { return this._isInSync; }
+        }
+
+        /// <summary>
+        /// All items of the tree.
+        /// </summary>
+        public IEnumerable<T> Items
+        {
+            get { return this._items; }
+        }
+
+        /// <summary>
+        /// Count of all items.
+        /// </summary>
+        public int Count
+        {
+            get { return this._items.Count; }
+        }
+
+        /// <summary>
+        /// Whether the tree should be rebuild automatically. Defaults to true.
+        /// </summary>
+        public bool AutoRebuild
+        {
+            get { return this._autoRebuild; }
+            set { this._autoRebuild = value; }
+        }
+
+        /// <summary>
+        /// Initializes an empty tree.
+        /// </summary>
+        public RangeTree(IComparer<T> rangeComparer = null) : this(null, rangeComparer) { }
+
+        /// <summary>
+        /// Initializes a tree with a list of items to be added.
+        /// </summary>
+        public RangeTree(IEnumerable<T> items, IComparer<T> rangeComparer = null)
+        {
+            this._rangeComparer = rangeComparer ?? new DefaultRangeComparer<TKey, T>();
+            this._items = items != null ? items.ToList() : new List<T>();
+            this._root = new RangeTreeNode<TKey, T>(this._items, rangeComparer);
+            this._isInSync = true;
+            this._autoRebuild = true;
+        }
+
+        /// <summary>
+        /// Performans a "stab" query with a single value.
+        /// All items with overlapping ranges are returned.
+        /// </summary>
+        public IEnumerable<T> Query(TKey value)
+        {
+            if (!this._isInSync && this._autoRebuild)
+                this.Rebuild();
+
+            return this._root.Query(value);
+        }
+
+        /// <summary>
+        /// Performans a range query.
+        /// All items with overlapping ranges are returned.
+        /// </summary>
+        public IEnumerable<T> Query(Range<TKey> range)
+        {
+            if (!this._isInSync && this._autoRebuild)
+                this.Rebuild();
+
+            return this._root.Query(range);
+        }
+
+        /// <summary>
+        /// Rebuilds the tree if it is out of sync.
+        /// </summary>
+        public void Rebuild()
+        {
+            if (this._isInSync)
+                return;
+
+            this._root = new RangeTreeNode<TKey, T>(this._items, this._rangeComparer);
+            this._isInSync = true;
+        }
+
+        /// <summary>
+        /// Adds the specified item. Tree will go out of sync.
+        /// </summary>
+        public void Add(T item)
+        {
+            this._isInSync = false;
+            this._items.Add(item);
+        }
+
+        /// <summary>
+        /// Adds the specified items. Tree will go out of sync.
+        /// </summary>
+        public void Add(IEnumerable<T> items)
+        {
+            this._isInSync = false;
+            this._items.AddRange(items);
+        }
+
+        /// <summary>
+        /// Removes the specified item. Tree will go out of sync.
+        /// </summary>
+        public void Remove(T item)
+        {
+            this._isInSync = false;
+            this._items.Remove(item);
+        }
+
+        /// <summary>
+        /// Removes the specified items. Tree will go out of sync.
+        /// </summary>
+        public void Remove(IEnumerable<T> items)
+        {
+            this._isInSync = false;
+
+            foreach (var item in items)
+                this._items.Remove(item);
+        }
+
+        /// <summary>
+        /// Clears the tree (removes all items).
+        /// </summary>
+        public void Clear()
+        {
+            this._root = new RangeTreeNode<TKey, T>(this._rangeComparer);
+            this._items = new List<T>();
+            this._isInSync = true;
+        }
+    }
+
+
+}
---
+++ Orc/Entities/RangeTree/RangeTreeNode.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MB.Algodat
+{
+    /// <summary>
+    /// A node of the range tree. Given a list of items, it builds
+    /// its subtree. Also contains methods to query the subtree.
+    /// Basically, all interval tree logic is here.
+    /// </summary>
+    public class RangeTreeNode<TKey, T>
+        where TKey : IComparable<TKey>
+        where T : IRangeProvider<TKey>
+    {
+        private TKey _center;
+        private RangeTreeNode<TKey, T> _leftNode;
+        private RangeTreeNode<TKey, T> _rightNode;
+        private List<T> _items;
+
+        /// <summary>
+        /// Initializes an empty node.
+        /// </summary>
+        /// <param name="rangeComparer">The comparer used to compare two items.</param>
+        public RangeTreeNode(IComparer<T> rangeComparer = null) : this(null, rangeComparer) { }
+
+        /// <summary>
+        /// Initializes a node with a list of items, builds the sub tree.
+        /// </summary>
+        /// <param name="items">Data to include in this node.</param>
+        /// <param name="rangeComparer">The comparer used to compare two items.</param>
+        public RangeTreeNode(IEnumerable<T> items, IComparer<T> rangeComparer = null)
+        {
+            rangeComparer = rangeComparer ?? new DefaultRangeComparer<TKey, T>();
+
+            // No items? nothing to do
+            if (items == null || items.Count() == 0) return;
+
+            // first, find the median
+            var endPoints = new List<TKey>();
+            foreach (var o in items)
+            {
+                var range = o.Range;
+                endPoints.Add(range.From);
+                endPoints.Add(range.To);
+            }
+            endPoints.Sort();
+
+            // the median is used as center value
+            _center = endPoints[endPoints.Count / 2];
+            _items = new List<T>();
+
+            var left = new List<T>();
+            var right = new List<T>();
+
+            // iterate over all items
+            // if the range of an item is completely left of the center, add it to the left items
+            // if it is on the right of the center, add it to the right items
+            // otherwise (range overlaps the center), add the item to this node's items
+            foreach (var o in items)
+            {
+                var range = o.Range;
+
+                if (range.To.CompareTo(_center) < 0)
+                    left.Add(o);
+                else if (range.From.CompareTo(_center) > 0)
+                    right.Add(o);
+                else
+                    _items.Add(o);
+            }
+
+            // sort the items, this way the query is faster later on
+            if (_items.Count > 0)
+                _items.Sort(rangeComparer);
+            else
+                _items = null;
+
+            // create left and right nodes, if there are any items
+            if (left.Count > 0)
+                _leftNode = new RangeTreeNode<TKey, T>(left);
+            if (right.Count > 0)
+                _rightNode = new RangeTreeNode<TKey, T>(right);
+        }
+
+        /// <summary>
+        /// Performans a "stab" query with a single value.
+        /// All items with overlapping ranges are returned.
+        /// </summary>
+        public IEnumerable<T> Query(TKey value)
+        {
+            // If the node has items, check their ranges.
+            if (_items != null)
+            {
+                var localSearch = _items
+                    .TakeWhile(o => o.Range.From.CompareTo(value) <= 0)
+                    .Where(o => o.Range.Contains(value));
+
+                foreach (var o in localSearch)
+                {
+                    yield return o;
+                }
+            }
+
+            // go to the left or go to the right of the tree, depending
+            // where the query value lies compared to the center
+            if (value.CompareTo(_center) < 0 && _leftNode != null)
+            {
+                foreach (var leftValue in _leftNode.Query(value))
+                {
+                    yield return leftValue;
+                }
+            }
+            else if (value.CompareTo(_center) > 0 && _rightNode != null)
+            {
+                foreach (var rightValue in _rightNode.Query(value))
+                {
+                    yield return rightValue;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Performans a range query.
+        /// All items with overlapping ranges are returned.
+        /// </summary>
+        public IEnumerable<T> Query(Range<TKey> range)
+        {
+            // If the node has items, check their ranges.
+            if (_items != null)
+            {
+                var localSearch = _items
+                    .TakeWhile(o => o.Range.From.CompareTo(range.To) <= 0)
+                    .Where(o => o.Range.Intersects(range));
+
+                foreach (var o in localSearch)
+                {
+                    yield return o;
+                }
+            }
+
+            // go to the left or go to the right of the tree, depending
+            // where the query value lies compared to the center
+            if (range.From.CompareTo(_center) < 0 && _leftNode != null)
+            {
+                foreach (var leftValue in _leftNode.Query(range))
+                {
+                    yield return leftValue;
+                }
+            }
+            if (range.To.CompareTo(_center) > 0 && _rightNode != null)
+            {
+                foreach (var rightValue in _rightNode.Query(range))
+                {
+                    yield return rightValue;
+                }
+            }
+        }
+    }
+}
