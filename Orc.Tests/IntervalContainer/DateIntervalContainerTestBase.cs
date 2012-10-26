a3516cf2bde4442992107ceb529a3ee66da3b4ec
Petro Sidlovskyy <sidlovskyy@gmail.com>
2012-10-26 23:02:27 +0300

    Moved existing tests to generic IntervalContainer test class. Some existing code tabulation fixed.

--- Orc.Tests/IntervalContainer/DateIntervalContainerTestBase.cs
+++ Orc.Tests/IntervalContainer/DateIntervalContainerTestBase.cs
@@ -1,50 +1,135 @@
 ﻿namespace Orc.Tests.IntervalContainer
 {
-	using System;
-	using System.Collections.Generic;
-	using System.Diagnostics;
-	using System.Linq;
-	using System.Text;
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Text;
 
-	using NUnit.Framework;
+    using NUnit.Framework;
 
-	using Orc.Entities;
-	using Orc.Interface;
+    using Orc.Entities;
+    using Orc.Interface;
 
-	public abstract class DateIntervalContainerTestBase
-	{
-		private Stopwatch stopwatch;
+    public abstract class DateIntervalContainerTestBase
+    {
+        private Stopwatch stopwatch;
 
-		private StringBuilder timeEllapsedReport;
+        private StringBuilder timeEllapsedReport;
 
-		private DateTime now;
+        private DateTime now;
 
-		protected DateTime inOneHour;
+        protected DateTime inOneHour;
 
-		protected DateTime inTwoHours;
+        protected DateTime inTwoHours;
 
-		protected DateTime inThreeHours;
+        protected DateTime inThreeHours;
 
-		protected abstract IIntervalContainer<DateTime> CreateIntervalContainer(IEnumerable<Interval<DateTime>> intervals);
+        protected abstract IIntervalContainer<DateTime> CreateIntervalContainer();
 
-		[SetUp]
-		public void Setup()
-		{
-			stopwatch = null;
+        [SetUp]
+        public void Setup()
+        {
+            stopwatch = null;
 			timeEllapsedReport = new StringBuilder();
 
-			now = DateTime.Now;
-			inOneHour = now.AddHours(1);
-			inTwoHours = now.AddHours(2);
-			inThreeHours = now.AddHours(3);
-		}
+            now = DateTime.Now;
+            inOneHour = now.AddHours(1);
+            inTwoHours = now.AddHours(2);
+            inThreeHours = now.AddHours(3);
 
+            Debug.WriteLine(string.Format("Now: {0}", now));
+        }
 
-		#region Benchmark	              
+        [TestCase(4, 4, 3)]
+        [TestCase(4, 5, 4)]
+        [TestCase(-1, 10, 7)]
+        [TestCase(-1, -1, 0)]
+        [TestCase(1, 4, 5)]
+        [TestCase(0, 1, 2)]
+        [TestCase(10, 12, 0)]
+        public void Query_ForDifferentDateIntervals_ShoudReturnCorrectOverlapsCount(int leftEdgeMinutes, int rightEdgeMinutes, int expectedOverlapsCount)
+        {
+            //Arrange
+            var intervals = new List<Interval<DateTime>>();
+
+            intervals.Add(ToDateTimeInterval(now, -300, -200));
+            intervals.Add(ToDateTimeInterval(now, -3, -2));
+            intervals.Add(ToDateTimeInterval(now, 1, 2));
+            intervals.Add(ToDateTimeInterval(now, 3, 6));
+            intervals.Add(ToDateTimeInterval(now, 2, 4));
+            intervals.Add(ToDateTimeInterval(now, 5, 7));
+            intervals.Add(ToDateTimeInterval(now, 1, 3));
+            intervals.Add(ToDateTimeInterval(now, 4, 6));
+            intervals.Add(ToDateTimeInterval(now, 8, 9));
+            intervals.Add(ToDateTimeInterval(now, 15, 20));
+            intervals.Add(ToDateTimeInterval(now, 40, 50));
+            intervals.Add(ToDateTimeInterval(now, 49, 60));
+
+            IIntervalContainer<DateTime> intervalContainer = this.CreateIntervalContainer(intervals);
+
+            //Act
+            var overlaps = intervalContainer.Query(ToDateTimeInterval(now, leftEdgeMinutes, rightEdgeMinutes));
+
+            //Assert
+            Assert.AreEqual(expectedOverlapsCount, overlaps.Count());
+        }
 
         [Test]
-		[Category("Benchmark")]
-		public void Query_Benchmark_Test()
+        public void Query_ForNullInterval_ShouldReturnEmptyList()
+        {
+            //Arrange
+            var intervals = new List<Interval<DateTime>>();
+            intervals.Add(new Interval<DateTime>(now, inOneHour));
+
+            var intervalContainer = CreateIntervalContainer(intervals);
+
+            //Act
+            var intersections = intervalContainer.Query(null);
+
+            //Assert
+            Assert.AreEqual(0, intersections.Count());
+        }
+
+        [Test]
+        public void Query_IntervalBetweenTwoInclusiveIntervals_ShouldReturnBothIntervals()
+        {
+            //Arrange
+            var intervals = new List<Interval<DateTime>>();
+            intervals.Add(new Interval<DateTime>(now, inOneHour, isMaxInclusive: true));
+            intervals.Add(new Interval<DateTime>(inTwoHours, inThreeHours, isMinInclusive: true));
+
+            var intervalContainer = CreateIntervalContainer(intervals);
+
+            //Act
+            var intersections = intervalContainer.Query(new Interval<DateTime>(inOneHour, inTwoHours));
+
+            //Assert
+            CollectionAssert.AreEquivalent(intervals, intersections.ToList());
+        }
+
+        [Test]
+        public void Query_IntervalBetweenTwoNotInclusiveIntervals_ShouldReturnEmptyList()
+        {
+            //Arrange
+            var intervals = new List<Interval<DateTime>>();
+            intervals.Add(new DateInterval(now, inOneHour, isMaxInclusive: false));
+            intervals.Add(new DateInterval(inTwoHours, inThreeHours, isMinInclusive: false));
+
+            var intervalContainer = CreateIntervalContainer(intervals);
+
+            //Act
+            var intersections = intervalContainer.Query(new Interval<DateTime>(inOneHour, inTwoHours));
+
+            //Assert
+            Assert.AreEqual(0, intersections.Count());
+        }
+
+        #region Benchmark
+
+        [Test]
+        [Category("Benchmark")]
+        public void Query_Benchmark_Test()
         {
             const int numberOfIntervals = 10000;
 
@@ -52,18 +137,18 @@
             
             stopwatch = Stopwatch.StartNew();
 
-			IIntervalContainer<DateTime> intervalContainer = CreateIntervalContainer(intervals);
+			var intervalContainer = this.CreateIntervalContainer(intervals);
 
             stopwatch.Stop();
-            timeEllapsedReport.AppendLine("Time taken to build tree: " + stopwatch.ElapsedMilliseconds);
+            timeEllapsedReport.AppendLine("Time taken to build data structure: " + stopwatch.ElapsedMilliseconds);
 
-			var result1 = TestSearchForInterval(now, now.AddMinutes(numberOfIntervals), intervalContainer, "first");
+            var result1 = TestSearchForInterval(now, now.AddMinutes(numberOfIntervals), intervalContainer, "first");
 
-			var result2 = TestSearchForInterval(now.AddMinutes(-1), now.AddMinutes(1), intervalContainer, "second");
+            var result2 = TestSearchForInterval(now.AddMinutes(-1), now.AddMinutes(1), intervalContainer, "second");
 
-			var result3 = TestSearchForInterval(now.AddMinutes(-numberOfIntervals), now.AddMinutes(numberOfIntervals), intervalContainer, "third");
+            var result3 = TestSearchForInterval(now.AddMinutes(-numberOfIntervals), now.AddMinutes(numberOfIntervals), intervalContainer, "third");
 
-			var result4 = TestSearchForInterval(now.AddMinutes(numberOfIntervals - 1), now.AddMinutes(numberOfIntervals), intervalContainer, "fourth");            
+            var result4 = TestSearchForInterval(now.AddMinutes(numberOfIntervals - 1), now.AddMinutes(numberOfIntervals), intervalContainer, "fourth");            
 
             Assert.AreEqual(numberOfIntervals, result1.Count());
 
@@ -77,7 +162,17 @@
             Debug.WriteLine(timeElapsedSummary);
         }
 
-		private IEnumerable<IInterval<DateTime>> TestSearchForInterval(DateTime startEdge, DateTime endEdge, IIntervalContainer<DateTime> searchIn, string testName)
+        private IIntervalContainer<DateTime> CreateIntervalContainer(IEnumerable<Interval<DateTime>> intervals)
+        {
+            IIntervalContainer<DateTime> intervalContainer = CreateIntervalContainer();
+            foreach (var interval in intervals)
+            {
+                intervalContainer.Add(interval);
+            }
+            return intervalContainer;
+        }
+
+        private IEnumerable<IInterval<DateTime>> TestSearchForInterval(DateTime startEdge, DateTime endEdge, IIntervalContainer<DateTime> searchIn, string testName)
         {
             stopwatch.Reset();
             stopwatch.Start();
@@ -101,5 +196,10 @@
             return new List<Interval<DateTime>>(dateRanges).OrderBy(x => x.Min.Value);
         }
 		#endregion
+
+        private static Interval<DateTime> ToDateTimeInterval(DateTime startTime, int leftEdgeMinutes, int rightEdgeMinutes)
+        {
+            return new Interval<DateTime>(startTime.AddMinutes(leftEdgeMinutes), startTime.AddMinutes(rightEdgeMinutes));
+        }
 	}
 }
---
+++ Orc.Tests/IntervalContainer/IntervalTreeVVondraTest.cs
@@ -0,0 +1,18 @@
+﻿namespace Orc.Tests.IntervalContainer
+{
+    using System;
+
+    using NUnit.Framework;
+
+    using Orc.Entities.IntervalTreeVvondra;
+    using Orc.Interface;
+
+    [TestFixture]
+    public class IntervalTreeVVondraTest : DateIntervalContainerTestBase
+    {
+        protected override IIntervalContainer<DateTime> CreateIntervalContainer()
+        {
+            return new IntervalTree<DateTime>();
+        }
+    }
+}
