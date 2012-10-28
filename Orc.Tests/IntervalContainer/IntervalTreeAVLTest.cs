fb7dd6801741fb67b51eb8596b422cf4f4bbf952
orcomp <orcomp@server>
2012-10-29 07:47:36 +1100

    Simplest failing code for IntervalTreeAVL

--- Orc.Tests/IntervalContainer/IntervalTreeAVLTest.cs
+++ Orc.Tests/IntervalContainer/IntervalTreeAVLTest.cs
@@ -1,9 +1,12 @@
 ﻿namespace Orc.Tests.IntervalContainer
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
 
     using NUnit.Framework;
 
+    using Orc.Entities;
     using Orc.Entities.IntervalTreeAVL;
     using Orc.Interface;
 
@@ -13,6 +16,41 @@
         protected override IIntervalContainer<DateTime> CreateIntervalContainer()
         {
             return new IntervalTree<DateTime>();
+        }
+
+        [Test]
+        public void SimplestFailingTest()
+        {
+            var now = DateTime.Now;
+
+            //Arrange
+            var intervals = new List<Interval<DateTime>>();
+            //intervals.Add(ToDateTimeInterval(now, -300, -200));
+            //intervals.Add(ToDateTimeInterval(now, -3, -2));
+            intervals.Add(ToDateTimeInterval(now, 1, 2));
+            //intervals.Add(ToDateTimeInterval(now, 3, 6));
+            //intervals.Add(ToDateTimeInterval(now, 2, 4));
+            //intervals.Add(ToDateTimeInterval(now, 5, 7));
+            intervals.Add(ToDateTimeInterval(now, 1, 3));
+            //intervals.Add(ToDateTimeInterval(now, 4, 6));
+            //intervals.Add(ToDateTimeInterval(now, 8, 9));
+            //intervals.Add(ToDateTimeInterval(now, 15, 20));
+            //intervals.Add(ToDateTimeInterval(now, 40, 50));
+            //intervals.Add(ToDateTimeInterval(now, 49, 60));
+
+            var intervalTree = new IntervalTree<DateTime>();
+            intervals.ForEach(x => intervalTree.Add(x));
+
+            //Act
+            var overlaps = intervalTree.Query(ToDateTimeInterval(now, 0, 1));
+
+            //Assert
+            Assert.AreEqual(2, overlaps.Count());
+        }
+
+        private static Interval<DateTime> ToDateTimeInterval(DateTime startTime, int leftEdgeMinutes, int rightEdgeMinutes, bool includeEdges = true)
+        {
+            return new Interval<DateTime>(startTime.AddMinutes(leftEdgeMinutes), startTime.AddMinutes(rightEdgeMinutes), includeEdges, includeEdges);
         }
     }
 }
