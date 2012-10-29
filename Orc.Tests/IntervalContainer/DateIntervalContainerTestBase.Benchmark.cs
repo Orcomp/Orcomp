cdb38f44d5d95515897e4d06ead001b008d201dc
Petro Sidlovskyy <sidlovskyy@gmail.com>
2012-10-30 01:30:08 +0200

    Benchmark test for add/remove added. Memory monitoring added.

--- Orc.Tests/IntervalContainer/DateIntervalContainerTestBase.Benchmark.cs
+++ Orc.Tests/IntervalContainer/DateIntervalContainerTestBase.Benchmark.cs
@@ -20,20 +20,15 @@
 
             var intervals = GetDateRangesAllDescendingEndTimes(now, numberOfIntervals).ToList();
 
-            stopwatch = Stopwatch.StartNew();
+            var intervalContainer = RunIntervalContainerBuild(intervals);
 
-            var intervalContainer = this.CreateIntervalContainer(intervals);
+            var result1 = TestQueryForInterval(ToDateTimeInterval(now, 0, numberOfIntervals), intervalContainer, "Mid Point to Max Spanning Interval");
 
-            stopwatch.Stop();
-            timeEllapsedReport.AppendLine(string.Format("Time taken to build data structure: {0} ms", this.stopwatch.ElapsedMilliseconds));
+            var result2 = TestQueryForInterval(ToDateTimeInterval(now, -1, 1), intervalContainer, "Mid Point +/- 1");
 
-            var result1 = this.TestQueryForInterval(ToDateTimeInterval(now, 0, numberOfIntervals), intervalContainer, "Mid Point to Max Spanning Interval");
+            var result3 = TestQueryForInterval(ToDateTimeInterval(now, -numberOfIntervals, numberOfIntervals), intervalContainer, "Min to Max Spanning Interval");
 
-            var result2 = this.TestQueryForInterval(ToDateTimeInterval(now, -1, 1), intervalContainer, "Mid Point +/- 1");
-
-            var result3 = this.TestQueryForInterval(ToDateTimeInterval(now, -numberOfIntervals, numberOfIntervals), intervalContainer, "Min to Max Spanning Interval");
-
-            var result4 = this.TestQueryForInterval(ToDateTimeInterval(now, numberOfIntervals - 1, numberOfIntervals), intervalContainer, "Max Spanning interval -1 to Max Spanning Interval");
+            var result4 = TestQueryForInterval(ToDateTimeInterval(now, numberOfIntervals - 1, numberOfIntervals), intervalContainer, "Max Spanning interval -1 to Max Spanning Interval");
 
             Assert.AreEqual(numberOfIntervals, result1.Count());
 
@@ -70,37 +65,126 @@
 
             var intervals = GetSequentialDateTimeIntervals(now, numberOfIntervals, intervalLength, spaceLength).ToList();
 
-            stopwatch = Stopwatch.StartNew();
+            var intervalContainer = RunIntervalContainerBuild(intervals);
 
-            var intervalContainer = this.CreateIntervalContainer(intervals);
+            RunQueryBenchmarkTestsForSquentialInterval(intervalContainer, intervalLength, intervalAndSpaceLength, numberOfIntervals);
+
+            var timeElapsedSummary = timeEllapsedReport.ToString();
+            Debug.WriteLine(timeElapsedSummary);
+        }
+
+        [Test]
+        [Category("Benchmark")]
+        [Timeout(60 * 1000)]
+        public void Add_Remove_BenchmarkSequentialIntervals_Test()
+        {
+            const int numberOfIntervals = 1000000;
+            const int intervalLength = 5;
+            const int spaceLength = 1;
+            const int intervalAndSpaceLength = intervalLength + spaceLength;            
+
+            var intervals = GetSequentialDateTimeIntervals(now, numberOfIntervals, intervalLength, spaceLength).ToList();
+
+            var intervalContainer = RunIntervalContainerBuild(intervals);
+
+            RunQueryBenchmarkTestsForSquentialInterval(intervalContainer, intervalLength, intervalAndSpaceLength, numberOfIntervals);
+
+            RemoveHalfOfIntervals(intervalContainer, intervals);
+
+            const int halfNumberOfIntervals = numberOfIntervals / 2;
+            RunQueryBenchmarkTestsForSquentialInterval(intervalContainer, intervalLength, intervalAndSpaceLength, halfNumberOfIntervals);
+
+            AddRemovedIntervals(intervalContainer, intervals);
+
+            RunQueryBenchmarkTestsForSquentialInterval(intervalContainer, intervalLength, intervalAndSpaceLength, numberOfIntervals);
+
+            var timeElapsedSummary = timeEllapsedReport.ToString();
+            Debug.WriteLine(timeElapsedSummary);
+        }
+
+        private void RemoveHalfOfIntervals(IIntervalContainer<DateTime> intervalContainer, List<Interval<DateTime>> intervals)
+        {
+            stopwatch.Reset();
+            stopwatch.Start();
+
+            //remove second part of intervals
+            int halfNumberOfIntervals = intervals.Count / 2;
+            for (int i = 0; i < halfNumberOfIntervals; i++)
+            {
+                intervalContainer.Remove(intervals[halfNumberOfIntervals + i]);
+            }
 
             stopwatch.Stop();
-            timeEllapsedReport.AppendLine(string.Format("Time taken to build data structure: {0} ms", this.stopwatch.ElapsedMilliseconds));
 
-            var beforeBeginnigResult = this.TestQueryForInterval(
-                ToDateTimeInterval(now, -2 * intervalLength, -intervalLength),
-                intervalContainer,
+            timeEllapsedReport.AppendLine();
+            timeEllapsedReport.AppendLine(string.Format("Time taken for removing {0} intervals: {1} ms", halfNumberOfIntervals, stopwatch.ElapsedMilliseconds));
+            timeEllapsedReport.AppendLine();
+        }
+
+        private void AddRemovedIntervals(IIntervalContainer<DateTime> intervalContainer, List<Interval<DateTime>> intervals)
+        {
+            stopwatch.Reset();
+            stopwatch.Start();
+
+            //add second part of intervals
+            int halfNumberOfIntervals = intervals.Count / 2;
+            for (int i = 0; i < halfNumberOfIntervals; i++)
+            {
+                intervalContainer.Add(intervals[halfNumberOfIntervals + i]);
+            }
+
+            stopwatch.Stop();
+
+            timeEllapsedReport.AppendLine();
+            timeEllapsedReport.AppendLine(string.Format("Time taken for adding {0} intervals: {1} ms", halfNumberOfIntervals, stopwatch.ElapsedMilliseconds));
+            timeEllapsedReport.AppendLine();
+        }        
+
+        private IIntervalContainer<DateTime> RunIntervalContainerBuild(IEnumerable<Interval<DateTime>> intervals)
+        {
+            long memStart = GC.GetTotalMemory(true);
+            stopwatch = Stopwatch.StartNew();            
+
+            var intervalContainer = CreateIntervalContainer(intervals);
+                        
+            stopwatch.Stop();
+            long memEnd = GC.GetTotalMemory(true);
+            long bytesUsed = memEnd - memStart;
+
+            timeEllapsedReport.AppendLine();
+            timeEllapsedReport.AppendLine(string.Format("Time taken to build data structure: {0} ms", stopwatch.ElapsedMilliseconds));
+            timeEllapsedReport.AppendLine(string.Format("Memory used to build data structure: {0} bytes", bytesUsed));
+            timeEllapsedReport.AppendLine();
+
+            return intervalContainer;
+        }
+
+        private void RunQueryBenchmarkTestsForSquentialInterval(IIntervalContainer<DateTime> intervalContainer, int intervalLength, int intervalAndSpaceLength, int numberOfIntervals)
+        {
+            var beforeBeginnigResult = TestQueryForInterval(
+                ToDateTimeInterval(now, -2 * intervalLength, -intervalLength), 
+                intervalContainer, 
                 "Before The Beginning");
 
-            var atTheBeginnigResult = this.TestQueryForInterval(
+            var atTheBeginnigResult = TestQueryForInterval(
                 ToDateTimeInterval(now, -1, 1), 
                 intervalContainer, 
                 "At The Beginning");
 
-            const int middleIntervalIndex = numberOfIntervals / 2;
-            var inTheMiddleResult = this.TestQueryForInterval(
-                ToDateTimeInterval(now, (middleIntervalIndex) * intervalAndSpaceLength, ((middleIntervalIndex) + 1) * intervalAndSpaceLength - 1), 
-                intervalContainer, 
+            int middleIntervalIndex = numberOfIntervals / 2;
+            var inTheMiddleResult = TestQueryForInterval(
+                ToDateTimeInterval(now, (middleIntervalIndex) * intervalAndSpaceLength, ((middleIntervalIndex) + 1) * intervalAndSpaceLength - 1),
+                intervalContainer,
                 "In The Middle");
 
-            var atTheEndResult = this.TestQueryForInterval(
-                ToDateTimeInterval(now, (numberOfIntervals - 1) * intervalAndSpaceLength, numberOfIntervals * intervalAndSpaceLength), 
-                intervalContainer, 
+            var atTheEndResult = TestQueryForInterval(
+                ToDateTimeInterval(now, (numberOfIntervals - 1) * intervalAndSpaceLength, numberOfIntervals * intervalAndSpaceLength),
+                intervalContainer,
                 "At The End");
 
-            var afterTheEndResult = this.TestQueryForInterval(
-                ToDateTimeInterval(now, (numberOfIntervals + 1) * intervalAndSpaceLength, (numberOfIntervals + 2) * intervalAndSpaceLength), 
-                intervalContainer, 
+            var afterTheEndResult = TestQueryForInterval(
+                ToDateTimeInterval(now, (numberOfIntervals + 1) * intervalAndSpaceLength, (numberOfIntervals + 2) * intervalAndSpaceLength),
+                intervalContainer,
                 "After The End");
 
             Assert.AreEqual(0, beforeBeginnigResult.Count());
@@ -112,9 +196,6 @@
             Assert.AreEqual(1, atTheEndResult.Count());
 
             Assert.AreEqual(0, afterTheEndResult.Count());
-
-            var timeElapsedSummary = timeEllapsedReport.ToString();
-            Debug.WriteLine(timeElapsedSummary);
         }
 
         private IEnumerable<Interval<DateTime>> GetSequentialDateTimeIntervals(DateTime startTime, int numberOfIntervals, int intervalLength, int spaceLength)
