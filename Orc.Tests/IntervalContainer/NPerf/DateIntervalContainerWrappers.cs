9f195935b2901a2e648705a345ff315196306268
Petro Sidlovskyy <sidlovskyy@gmail.com>
2012-11-15 02:21:26 +0200

    Concrete implementations of generic interval containers added as NPerf doesn't support generics.

---
+++ Orc.Tests/IntervalContainer/NPerf/DateIntervalContainerWrappers.cs
@@ -0,0 +1,9 @@
+﻿namespace Orc.Tests.IntervalContainer.NPerf
+{
+    using System;
+    
+    //concrete implementations of generic classes under test are required here because NPerf doesn't support generics 
+    public class DateRBIntervalTree : Orc.Entities.IntervalTreeRB.IntervalTree<DateTime> { }
+    public class DateAVLIntervalTree : Orc.Entities.IntervalTreeAVL.IntervalTree<DateTime> { }
+    public class DateRangeIntervalTree : Orc.Entities.RangeTree.RangeTree<DateTime> { }
+}
