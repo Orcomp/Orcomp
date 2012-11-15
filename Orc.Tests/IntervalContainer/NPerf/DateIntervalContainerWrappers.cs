62fa439170e569932d3c1227f65457b4b1f556b5
Petro Sidlovskyy <sidlovskyy@gmail.com>
2012-11-15 02:22:01 +0200

    Previous commit refactored.

--- Orc.Tests/IntervalContainer/NPerf/DateIntervalContainerWrappers.cs
+++ Orc.Tests/IntervalContainer/NPerf/DateIntervalContainerWrappers.cs
@@ -3,7 +3,7 @@
     using System;
     
     //concrete implementations of generic classes under test are required here because NPerf doesn't support generics 
-    public class DateRBIntervalTree : Orc.Entities.IntervalTreeRB.IntervalTree<DateTime> { }
-    public class DateAVLIntervalTree : Orc.Entities.IntervalTreeAVL.IntervalTree<DateTime> { }
-    public class DateRangeIntervalTree : Orc.Entities.RangeTree.RangeTree<DateTime> { }
+    public class DateRBIntervalTree : Entities.IntervalTreeRB.IntervalTree<DateTime> { }
+    public class DateAVLIntervalTree : Entities.IntervalTreeAVL.IntervalTree<DateTime> { }
+    public class DateRangeIntervalTree : Entities.RangeTree.RangeTree<DateTime> { }
 }
