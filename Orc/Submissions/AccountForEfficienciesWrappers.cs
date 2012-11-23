c975713a4b2d55ea1bf54066acb815c158492ad6
Petro Sidlovskyy <sidlovskyy@gmail.com>
2012-11-24 01:46:24 +0200

    Wrappers over submissions added to make possible run extension methods for NPerf.

---
+++ Orc/Submissions/AccountForEfficienciesWrappers.cs
@@ -0,0 +1,54 @@
+﻿namespace Orc.Submissions
+{
+    using System.Collections.Generic;
+
+    using Orc.Entities;
+
+    public class vrstks_AccountForEfficiencies : IAccountForEfficienciesWrapper
+    {
+        public DateInterval AccountForEfficiencies(DateInterval initialInterval, List<DateIntervalEfficiency> dateIntervalEfficiencies)
+        {
+            return initialInterval.vrstks(dateIntervalEfficiencies);
+        }
+    }
+
+    public class vrstks2_AccountForEfficiencies : IAccountForEfficienciesWrapper
+    {
+        public DateInterval AccountForEfficiencies(DateInterval initialInterval, List<DateIntervalEfficiency> dateIntervalEfficiencies)
+        {
+            return initialInterval.vrstks2(dateIntervalEfficiencies);
+        }
+    }
+
+    public class thcristo_AccountForEfficiencies : IAccountForEfficienciesWrapper
+    {
+        public DateInterval AccountForEfficiencies(DateInterval initialInterval, List<DateIntervalEfficiency> dateIntervalEfficiencies)
+        {
+            return initialInterval.thcristo(dateIntervalEfficiencies);
+        }
+    }
+
+    public class thcristo2_AccountForEfficiencies: IAccountForEfficienciesWrapper
+    {
+        public DateInterval AccountForEfficiencies(DateInterval initialInterval, List<DateIntervalEfficiency> dateIntervalEfficiencies)
+        {
+            return initialInterval.thcristo2(dateIntervalEfficiencies);
+        }
+    }
+
+    public class Quicks01lver2_AccountForEfficiencies : IAccountForEfficienciesWrapper
+    {
+        public DateInterval AccountForEfficiencies(DateInterval initialInterval, List<DateIntervalEfficiency> dateIntervalEfficiencies)
+        {
+            return initialInterval.Quicks01lver2(dateIntervalEfficiencies);
+        }
+    }
+
+    public class MoustafaS_AccountForEfficiencies : IAccountForEfficienciesWrapper
+    {
+        public DateInterval AccountForEfficiencies(DateInterval initialInterval, List<DateIntervalEfficiency> dateIntervalEfficiencies)
+        {
+            return initialInterval.MoustafaS(dateIntervalEfficiencies);
+        }
+    }
+}
---
+++ Orc/Submissions/IAccountForEfficienciesWrapper.cs
@@ -0,0 +1,11 @@
+﻿namespace Orc.Submissions
+{
+    using System.Collections.Generic;
+
+    using Orc.Entities;
+
+    public interface IAccountForEfficienciesWrapper
+    {
+        DateInterval AccountForEfficiencies(DateInterval initialInterval, List<DateIntervalEfficiency> dateIntervalEfficiencies);
+    }
+}
