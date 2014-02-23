b9a1ef8bc8e5590c1e2efc847e1babb4489ed632
orcomp <orcomp@server>
2014-02-23 11:44:14 +1100

    (*) Improved Test and added performance test

--- src/Orc.Tests/LineSeriesTest.cs
+++ src/Orc.Tests/LineSeriesTest.cs
@@ -10,6 +10,7 @@ namespace Orc.Tests
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
 
     using NUnit.Framework;
 
@@ -23,15 +24,36 @@ namespace Orc.Tests
         [Test]
         public void InterpolateTest()
         {
-            var points = new List<Point>() { new Point(0, 0), new Point(10, 10), new Point(30, -10), new Point(40, 0) };
+            var points = new List<Point>() { new Point(0, 0), new Point(10, 10), new Point(30, -10), new Point(50, 10) };
 
             var lineSeries = new LineSeries(points);
 
-            var result = lineSeries.Interpolate(new Double[] { 0, 5, 10, 15, 20, 25, 35 });
+            var result = lineSeries.Interpolate(new Double[] { 0, 5, 10, 15, 20, 25, 35, 40, 45 });
 
-            var expectedResult = new List<Point>() { new Point(0,0), new Point(5,5), new Point(10,10), new Point(15,5), new Point(20, 0), new Point(25, -5), new Point(35,-5)};
+            var expectedResult = new List<Point>()
+                                     {
+                                         new Point(0,0), 
+                                         new Point(5,5), 
+                                         new Point(10,10), 
+                                         new Point(15,5), 
+                                         new Point(20, 0), 
+                                         new Point(25, -5), 
+                                         new Point(35,-5),
+                                         new Point(40,0),
+                                         new Point(45,5)
+                                     };
 
             CollectionAssert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void InterpolatePerformance1()
+        {
+            var points = new List<Point>() { new Point(0, 0), new Point(1000000, 1000000)};
+
+            var lineSeries = new LineSeries(points);
+
+            var result = lineSeries.Interpolate(Enumerable.Range(1, 999999).Select(x => x * 1.0));
         }
          
     }
