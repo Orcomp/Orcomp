15607ce436d10839716e65b1137759bc2903c9ed
orcomp <orcomp@server>
2014-02-23 11:24:07 +1100

    (+) Added LineSeries and Test

---
+++ src/Orc.Tests/LineSeriesTest.cs
@@ -0,0 +1,38 @@
+﻿// // --------------------------------------------------------------------------------------------------------------------
+// // <copyright file="IDateInterval.cs" company="ORC">
+// //   MS-PL
+// // </copyright>
+// // <summary>
+// //  
+// // </summary>
+// // --------------------------------------------------------------------------------------------------------------------
+namespace Orc.Tests
+{
+    using System;
+    using System.Collections.Generic;
+
+    using NUnit.Framework;
+
+    using System.Windows;
+
+    using Orc.Utilities;
+
+    [TestFixture]
+    public class LineSeriesTest
+    {
+        [Test]
+        public void InterpolateTest()
+        {
+            var points = new List<Point>() { new Point(0, 0), new Point(10, 10), new Point(30, -10), new Point(40, 0) };
+
+            var lineSeries = new LineSeries(points);
+
+            var result = lineSeries.Interpolate(new Double[] { 0, 5, 10, 15, 20, 25, 35 });
+
+            var expectedResult = new List<Point>() { new Point(0,0), new Point(5,5), new Point(10,10), new Point(15,5), new Point(20, 0), new Point(25, -5), new Point(35,-5)};
+
+            CollectionAssert.AreEqual(expectedResult, result);
+        }
+         
+    }
+}
---
+++ src/Orc/Utilities/LineSeries.cs
@@ -0,0 +1,46 @@
+﻿// // --------------------------------------------------------------------------------------------------------------------
+// // <copyright file="IDateInterval.cs" company="ORC">
+// //   MS-PL
+// // </copyright>
+// // <summary>
+// //  
+// // </summary>
+// // --------------------------------------------------------------------------------------------------------------------
+namespace Orc.Utilities
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Windows;
+
+    public class LineSeries
+    {
+        IEnumerable<Point> Points { get; set; }
+
+        /// <summary>
+        ///  Points must be sorted by their x value.
+        /// </summary>
+        /// <param name="points"></param>
+        public LineSeries(IEnumerable<Point> points )
+        {
+            Debug.Assert(points.Count() >= 2);
+
+            Points = points;
+        }
+
+        /// <summary>
+        /// Assume xValues to be sorted
+        /// </summary>
+        /// <param name="xValues"></param>
+        /// <returns></returns>
+        public IEnumerable<Point> Interpolate(IEnumerable<double> xValues)
+        {
+            Debug.Assert(Points.First().X >= xValues.First());
+            Debug.Assert(Points.Last().X <= xValues.Last());
+
+            throw new NotImplementedException();
+        }
+    }
+}
