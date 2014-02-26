d2c6c20c2d0359074f5a2ad1de5331388266c246
Bartosz Wróblewski <bawr@hszm.pl>
2014-02-26 17:04:45 +0100

    (*) LineSeries Interpolation: squashed commit

--- src/Orc.Tests/LineSeriesTest.cs
+++ src/Orc.Tests/LineSeriesTest.cs
@@ -19,7 +19,7 @@ namespace Orc.Tests
     using System.Windows;
 
     using Orc.DataStructures.AList;
-    using Orc.Linq;
+//  using Orc.Linq;
     using Orc.Utilities;
 
     [TestFixture]
@@ -31,6 +31,8 @@ namespace Orc.Tests
             var points = new List<Point>() { new Point(0, 0), new Point(10, 10), new Point(30, -10), new Point(50, 10) };
 
             var lineSeries = new LineSeries(points);
+
+            var data = new int[] { 1, 2, 3 };
 
             var result = lineSeries.Interpolate(new Double[] { 0, 5, 10, 15, 20, 25, 35, 40, 45 });
 
@@ -50,35 +52,71 @@ namespace Orc.Tests
             CollectionAssert.AreEqual(expectedResult, result);
         }
 
-        [Test]
-        public void InterpolatePerformance1()
+        [TestCase(LineSeries.SearchType.Best)]
+        [TestCase(LineSeries.SearchType.Binary)]
+        [TestCase(LineSeries.SearchType.Linear)]
+        public void InterpolatePerformance2(LineSeries.SearchType search_type)
         {
-            var points = new List<Point>() { new Point(0, 0), new Point(1000000, 1000000)};
+            var valueGroups = Enumerable.Range(0, 1000001).Select(x => x * 1.0).ToLookup(x => ((int)x) % 2);
 
-            var lineSeries = new LineSeries(points);
+            var interPoints = valueGroups[1].ToList();
+            var valuePoints = valueGroups[0].Select(x => new Point(x, x)).ToList();
 
-            var result = lineSeries.Interpolate(Enumerable.Range(1, 999999).Select(x => x * 1.0)).ToList();
+            var series = new LineSeries(valuePoints);
+            var result = series.Interpolate(interPoints, search_type).ToList();
         }
 
-        [Test]
-        public void InterpolatePerformance2()
+        [TestCase(LineSeries.SearchType.Best)]
+        [TestCase(LineSeries.SearchType.Binary)]
+        [TestCase(LineSeries.SearchType.Linear)]
+        public void InterpolatePerformance1(LineSeries.SearchType search_type)
         {
-            var xValues = new List<double>();
-            var evenPoints = new List<Point>();
+            var valuePoints = new List<Point>() {
+                new Point(0, 0),
+                new Point(1000000, 1000000),
+            };
+            var interPoints = Enumerable.Range(1, 999999).Select(x => x * 1.0).ToList();
 
-            foreach (var value in Enumerable.Range(0, 1000000).Select(x => x * 1.0))
+            var series = new LineSeries(valuePoints);
+            var result = series.Interpolate(interPoints, search_type).ToList();
+        }
+
+        [TestCase(LineSeries.SearchType.Best)]
+        [TestCase(LineSeries.SearchType.Binary)]
+        [TestCase(LineSeries.SearchType.Linear)]
+        public void InterpolatePerformance2(LineSeries.SearchType search_type)
+        {
+            var valueGroups = Enumerable.Range(0, 1000001).Select(x => x * 1.0).ToLookup(x => ((int)x) % 2);
+
+            var interPoints = valueGroups[1].ToList();
+            var valuePoints = valueGroups[0].Select(x => new Point(x, x)).ToList();
+
+            var series = new LineSeries(valuePoints);
+            var result = series.Interpolate(interPoints, search_type).ToList();
+        }
+
+        [TestCase(LineSeries.SearchType.Best)]
+        [TestCase(LineSeries.SearchType.Binary)]
+        [TestCase(LineSeries.SearchType.Linear)]
+        public void InterpolatePerformance3(LineSeries.SearchType search_type)
+        {
+            var randNumbers = new Random(0);
+
+            var interPoints = new List<double>();
+            var valuePoints = new List<Point>();
+
+            for (int i = 0; i < 100000; i++)
             {
-                if (value % 2 == 0)
-                {
-                    evenPoints.Add(new Point(value, value));
-                }
-
-                xValues.Add(value);
+                interPoints.Add(interPoints.LastOrDefault() + 100 * randNumbers.NextDouble());
             }
 
-            var lineSeries = new LineSeries(evenPoints);
+            for (int i = 0; i < 10000000; i++)
+            {
+                valuePoints.Add(new Point(valuePoints.LastOrDefault().X + randNumbers.NextDouble(), randNumbers.NextDouble()));
+            }
 
-            var result = lineSeries.Interpolate(xValues).ToList();
+            var series = new LineSeries(valuePoints);
+            var result = series.Interpolate(interPoints, search_type).ToList();
         }
     }
 }
--- src/Orc/Utilities/LineSeries.cs
+++ src/Orc/Utilities/LineSeries.cs
@@ -17,17 +17,25 @@ namespace Orc.Utilities
 
     public class LineSeries
     {
-        IEnumerable<Point> Points { get; set; }
+        public IEnumerable<Point> Points { get; private set; }
+
+        protected Point[] PointsArray;
+        protected List<Point> PointsList;
+        protected IList<Point> PointsIndexed;
 
         /// <summary>
         ///  Points must be sorted by their x value.
         /// </summary>
         /// <param name="points"></param>
-        public LineSeries(IEnumerable<Point> points )
+        public LineSeries(IEnumerable<Point> points)
         {
             Debug.Assert(points.Count() >= 2);
-
+            
             Points = points;
+
+            PointsArray = points as Point[];
+            PointsList = points as List<Point>;
+            PointsIndexed = points as IList<Point>;
         }
 
         /// <summary>
@@ -35,12 +43,230 @@ namespace Orc.Utilities
         /// </summary>
         /// <param name="xValues"></param>
         /// <returns></returns>
-        public IEnumerable<Point> Interpolate(IEnumerable<double> xValues)
+        public IEnumerable<Point> Interpolate(IEnumerable<double> xValues, SearchType search_type = SearchType.Best)
         {
-            Debug.Assert(Points.First().X >= xValues.First());
-            Debug.Assert(Points.Last().X <= xValues.Last());
+            switch (search_type)
+            {
+                case SearchType.Best:
+                    if (PointsArray != null)
+                        return InterpolateBinary(xValues, SearchArray);
+                    else if (PointsList != null)
+                        return InterpolateBinary(xValues, SearchList);
+                    else
+                        return InterpolateLinear(xValues);
 
-            throw new NotImplementedException();
+                case SearchType.Binary:
+                    if (PointsArray != null)
+                        return InterpolateBinary(xValues, SearchArray);
+                    else if (PointsList != null)
+                        return InterpolateBinary(xValues, SearchList);
+                    else
+                        throw new ArgumentException();
+                
+                case SearchType.Linear:
+                    return InterpolateLinear(xValues);
+                
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        private int SearchArray(double value, int start_index = 0)
+        {
+            var value_point = new Point(value, 0.0);
+            return SearchArray(value_point, start_index);
+        }
+
+        private int SearchArray(Point value, int start_index)
+        {
+            return Array.BinarySearch<Point>(PointsArray, start_index, PointsArray.Length - start_index, value, PointXComparer.Instance);
+        }
+
+        private int SearchList(double value, int start_index = 0)
+        {
+            var value_point = new Point(value, 0.0);
+            return SearchList(value_point, start_index);
+        }
+
+        private int SearchList(Point value, int start_index)
+        {
+            return PointsList.BinarySearch(start_index, PointsList.Count - start_index, value, PointXComparer.Instance);
+        }
+
+        private IEnumerable<Point> InterpolateBinary(IEnumerable<double> values, Func<double, int, int> search_func)
+        {
+            var iter_values = values.GetEnumerator();
+            var prev_value_x = Double.MinValue;
+            var curr_value_X = Double.MaxValue;
+
+            var points_index = 0;
+
+            // TODO - do we want custom exceptions for too small collections?
+
+            while (iter_values.MoveNext())
+            {
+                curr_value_X = iter_values.Current;
+
+                if (curr_value_X < prev_value_x)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+
+                points_index = search_func(curr_value_X, points_index);
+
+                if (points_index >= 0)
+                {
+                    yield return PointsIndexed[points_index];
+                }
+                else
+                {
+                    points_index = ~points_index - 1;
+                    var prev_point = PointsIndexed[points_index];
+                    var next_point = PointsIndexed[points_index + 1];
+                    yield return new Point(curr_value_X,
+                                           prev_point.Y + (next_point.Y - prev_point.Y)
+                                                        * (curr_value_X - prev_point.X)
+                                                        / (next_point.X - prev_point.X));
+                }
+
+                prev_value_x = iter_values.Current;
+            }
+        }
+
+        private IEnumerable<Point> InterpolateLinear(IEnumerable<double> values)
+        {
+            var iter_values = values.GetEnumerator();
+            var prev_value_x = Double.MinValue;
+            var curr_value_X = Double.MaxValue;
+
+            var iter_points = Points.GetEnumerator();
+            var prev_point = new Point();
+            var next_point = new Point();
+
+            // TODO - do we want custom exceptions for too small collections?
+
+            iter_points.MoveNext();
+            prev_point = iter_points.Current;
+
+            iter_points.MoveNext();
+            next_point = iter_points.Current;
+
+            while (iter_values.MoveNext())
+            {
+                curr_value_X = iter_values.Current;
+
+                if (curr_value_X < prev_value_x)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+
+                while (next_point.X < curr_value_X)
+                {
+                    prev_point = iter_points.Current;
+
+                    if (iter_points.MoveNext())
+                    {
+                        next_point = iter_points.Current;
+                    }
+                    else
+                    {
+                        throw new ArgumentOutOfRangeException();
+                    }
+                }
+
+                yield return new Point(curr_value_X,
+                                       prev_point.Y + (next_point.Y - prev_point.Y)
+                                                    * (curr_value_X - prev_point.X)
+                                                    / (next_point.X - prev_point.X));
+
+                prev_value_x = iter_values.Current;
+            }
+        }
+
+        public enum SearchType
+        {
+            Best,
+            Binary,
+            Linear,
         }
     }
+
+    public sealed class PointXComparer : IComparer<Point>
+    {
+        public static readonly PointXComparer Instance = new PointXComparer();
+
+        private static Comparer<double> double_cmp = Comparer<double>.Default;
+
+        public int Compare(Point x, Point y)
+        {
+ 	        return double_cmp.Compare(x.X, y.X);
+        }
+    }
+
+/*
+    public class EnumeratorProxyWithPrevious<T> : IEnumerator<T>
+    {
+        protected IEnumerator<T> BaseEnumerator { get; set; }
+        protected bool HasMoved { get; set; }
+
+        public EnumeratorProxyWithPrevious(IEnumerator<T> enumerator)
+        {
+            BaseEnumerator = enumerator;
+        }
+
+        public EnumeratorProxyWithPrevious(IEnumerable<T> enumerable)
+        {
+            BaseEnumerator = enumerable.GetEnumerator();
+        }
+
+        public T Current
+        {
+            get
+            {
+                return BaseEnumerator.Current;
+            }
+        }
+
+        object IEnumerator.Current
+        {
+            get
+            {
+                return BaseEnumerator.Current;
+            }
+        }
+
+        public void Dispose()
+        {
+            BaseEnumerator.Dispose();
+        }
+
+        public bool MoveNext()
+        {
+            if (HasMoved)
+            {
+                Previous = Current;
+            }
+            else
+            {
+                Previous = default(T);
+                HasMoved = true;
+            }
+
+            return BaseEnumerator.MoveNext();
+        }
+
+        public void Reset()
+        {
+            BaseEnumerator.Reset();
+            Previous = default(T);
+            HasMoved = false;
+        }
+
+        public T Previous
+        {
+            get;
+            private set;
+        }
+    }
+*/
 }
